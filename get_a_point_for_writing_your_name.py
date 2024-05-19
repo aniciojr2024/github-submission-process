b830493c6df62d7f1968bf304dd92a03f6c3d52f
Jose Anicio Junior <1015060160@georgebrown.ca>
2024-05-19 13:00:00 -0400

    Complete function to return my name and add tests

---
+++ get_a_point_for_writing_your_name.py
@@ -0,0 +1,6 @@
+def hi_my_name_is():
+    return "Jose Anicio Pereira Junior"  # Write your name inside the quotation marks
+
+# Call the function and print the result
+print(hi_my_name_is())
+pytest
---
+++ test_get_a_point_for_writing_your_name.py
@@ -0,0 +1,4 @@
+from get_a_point_for_writing_your_name import get_name
+
+def test_get_name():
+    assert get_name() == "Jose Anicio Junior"
